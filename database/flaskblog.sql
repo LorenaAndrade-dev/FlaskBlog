72ec42fef3b5635be5ce3743f21ed337a9fbae4a
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-09-20 11:47:57 -0300

    Aula 09

--- database/flaskblog.sql
+++ database/flaskblog.sql
@@ -134,12 +134,251 @@ CREATE TABLE comment(
     );
     -- Atualiza type do staff --
 UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'admin' WHERE (`emp_id` = '1');
-UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' WHERE (`emp_id` = '3');
-UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' WHERE (`emp_id` = '4');
-UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' WHERE (`emp_id` = '5');
+UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' , emp_status = 'off' WHERE (`emp_id` = '3');
+UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' , emp_status = 'del' WHERE (`emp_id` = '4');
+UPDATE `flaskblogdb`.`staff` SET `emp_type` = 'author' , emp_status = 'off' WHERE (`emp_id` = '5');
 
--- Mostra registros da tabela 'Staff' --
+-- Tabela "article" --
+INSERT INTO article (
+    art_author,
+    art_title,
+    art_resume,
+    art_thumbnail,
+    art_content
+) VALUES (
+	'2',
+    'Primeiro artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'3',
+    'Segundo  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'4',
+    'Terceiro  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'1',
+    'Quarto  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'4',
+    'Quinto  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'3',
+    'Sexto  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'5',
+    'Setimo  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'1',
+    'Oitavo  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'4',
+    'Nono  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+),(
+	'6',
+    'Décimo  artigo',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'https://picsum.photos/300',
+    '
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum, dolor ad alias nesciunt consequuntur in error enim numquam sit sunt! Quia eius tempora provident tempore culpa cupiditate sunt dignissimos?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem qui voluptatum hic repudiandae labore quod deleniti temporibus perferendis quisquam recusandae, eum alias natus, dolor at! Error saepe est cupiditate consectetur!</p>
+<img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi consequatur laboriosam reiciendis dolorem, exercitationem dolor natus hic quisquam itaque maxime doloribus. Adipisci debitis quod perferendis repudiandae similique quam voluptatum eveniet.</p>
+<h4>Links:</h4>
+<ul>
+    <li><a href="https://catabist.com.br">Site do Fessô</a></li>
+    <li><a href="https://github.com/Luferat">Github do fessô</a></li>
+</ul>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis a mollitia doloribus repudiandae incidunt ullam debitis, minima iure quia, recusandae odio magnam velit quos ad nam eaque. Ut, dolorem eveniet?</p>    
+    '
+);
+
+-- Tabela 'comment' --
+INSERT INTO comment(
+-- Insira um "art_id" existente --
+	com_article,
+    com_author_name,
+    com_author_email,
+    com_comment
+) VALUES (
+	'10',
+    'Marlinelson',
+    'marli@nelson.com',
+    'É só mais um lorem ipsum.'
+),(
+	'4',
+    'Marluce',
+    'marlu@email.com',
+    'É isso mesmo.'
+),(
+	'3',
+    'Euclides',
+    'clidesu@email.com',
+    'Lorem ipsum dolor sit.'
+),(
+	'7',
+    'Soraia',
+    'soso@email.com',
+    'Lorem ipsum dolor sit.'
+),(
+	'10',
+    'Marcio',
+    'marcinho@email.com',
+    'Lorem ipsum dolor sit.'
+);
+
+-- ----------------------------------- --
+-- Testes de SELECT                    --
+-- Dica: execute uma linha de cada vez --
+-- ----------------------------------- --
+
+-- Mostra todos os registros da tabela 'staff' --
 SELECT * FROM staff;
+
+SELECT * FROM staff WHERE emp_id = '5';
+
+SELECT * FROM staff WHERE emp_name = 'Irineu Folha';
+
+-- Troque entre 'DESC' E 'ASC' para testar a ordem
+SELECT * FROM staff ORDER BY emp_name DESC;
+
+-- Somente autores
+SELECT emp_id, emp_name, emp_email FROM staff 
+	WHERE emp_type = 'author' 
+	ORDER BY emp_name;
+
+
+-- Mostra todos os registros da tabela 'article' --
+SELECT * FROM article;
+
+-- Mostra todos os registro da tabela 'comment'--
+SELECT * FROM comment;
+
+
 
 
 
